ac995dd95339e18874577629eebac0dde8245409
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2019-03-16 23:17:57 +0300

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/styles.css">	
+		<title>PHPGame Template</title>
+	</head>
+	<body>
+		<div class="container">
+			<div class="top">
+				<h1 class="geme-caption">Название игры</h1>
+			</div>
+			
+			<div class="name">Регистрация</div>
+			
+			<div class="content">
+				<p>Шаблон классической экономической игры на PHP</p>
+			</div>
+			
+			<div class="line">
+				<a href="index.html">Главная</a>
+			</div>	
+
+			<form method="POST" action="">	
+				<input type="text" name="login" placeholder="Ваше имя" required /><br>
+				<input type="password" name="password" placeholder="Введите пароль" required /><br>
+				<input type="password" name="verification" placeholder="Подтвердите пароль" required /><br>
+				<input type="submit" value="Регистрация" />			
+			</form>
+
+			<div class="copyright">
+				<span>&copy; 2012-2019 Все права защищены. Возрастное ограничение 12+</span>	
+			</div>
+		</div>					
+	</body>
+</html>	
